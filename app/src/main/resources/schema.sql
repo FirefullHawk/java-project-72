727c7a3798fc7474668515870dfbcaebd3e11abb
Dmitry Simanov <szizni1@gmail.com>
2023-09-19 22:36:10 +0300

    Fix trouble

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS urls;
 
 create table urls (
-  id                            SERIAL PRIMARY KEY,
-  name                          varchar(255) not null,
-  created_at                    timestamp not null
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP
 );
 
 DROP TABLE IF EXISTS url_checks;
 
 create table url_checks (
-  id                            SERIAL PRIMARY KEY,
-  status_code                   int not null,
-  title                         varchar(255),
-  h1                            varchar(255),
-  description                   text,
-  url_id                        int,
-  created_at                    timestamp not null
+  id SERIAL PRIMARY KEY,
+  status_code INT NOT NULL,
+  title varchar(255),
+  h1 varchar(255),
+  description text,
+  url_id INT,
+  created_at TIMESTAMP
 );
