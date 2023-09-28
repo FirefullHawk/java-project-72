ac0b9b419866309916f4da695111661b9949a8b3
Dmitry Simanov <szizni1@gmail.com>
2023-09-28 08:38:34 +0300

    Fix linter trouble

--- app/src/main/java/hexlet/code/utils/NamedRoutes.java
+++ app/src/main/java/hexlet/code/utils/NamedRoutes.java
@@ -4,11 +4,11 @@ public class NamedRoutes {
 
     private static final String ROOT_PATH = "/";
 
-    private static String URL_PATH;
-
     private static final String URLS_PATH = "/urls";
 
-    private static String URL_CHECK_PATH;
+    private static final String URL_PATH = URLS_PATH + "/%s";
+
+    private static final String URL_CHECK_PATH = URLS_PATH + "/%s/checks";
 
     public static String rootPath() {
         return ROOT_PATH;
@@ -23,13 +23,11 @@ public class NamedRoutes {
     }
 
     public static String urlPath(String id) {
-        URL_PATH = String.format(URLS_PATH + "/%s", id);
-        return URL_PATH;
+        return String.format(URL_PATH, id);
     }
 
     public static String urlCheckPath(String id) {
-        URL_CHECK_PATH = String.format(URLS_PATH + "/%s/checks", id);
-        return URL_CHECK_PATH;
+        return String.format(URL_CHECK_PATH, id);
     }
 
     public static String urlCheckPath(long id) {
